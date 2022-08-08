abab1d4ff8fcd57f970bc299eeeb579cc33e2cf4
dvbozhko@gmail.com <dvbozhko@gmail.com>
2022-08-08 18:12:07 +0200

    [MOVED] HH implementation moved to its own experiment

--- mads-experiments/src/main/kotlin/HHExperiment.kt
+++ mads-experiments/src/main/kotlin/HHExperiment.kt
@@ -1,29 +1,81 @@
 package domain
 
+import domain.mechanisms.*
 import domain.objects.HHCellObject
+import domain.objects.HHSignals
 import org.jetbrains.research.mads.core.configuration.Configuration
 import org.jetbrains.research.mads.core.configuration.Pathway
 import org.jetbrains.research.mads.core.simulation.Model
+import java.io.File
 
 fun main() {
     createHHCellsExperiment()
+    createDynamicExperimentMultipleI()
 }
 
 fun createHHCellsExperiment()
 {
-//    val hhCell = HHCellObject(HHSignals(-65.0, .0, .0, .0))
+    val I_exp = 8.0
+    val dynamic = HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6))
+
+    val config = Configuration()
+
+    val pathwayDynamic: Pathway<HHCellObject> = Pathway()
+    pathwayDynamic.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+
+    config.add(HHCellObject::class, arrayListOf(pathwayDynamic))
+
+    val s = Model(arrayListOf(dynamic), config)
+    s.simulate { it.currentTime() > 100000 }
+
+    var fname = "i_${I_exp}.txt"
+
+    File(fname).bufferedWriter().use { out ->
+        out.write("I;V;N;M;H\n")
+        dynamic.history.forEach {
+            it as HHSignals
+            out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
+        }
+    }
+}
+
+fun createDynamicExperimentMultipleI() {
+    for(iv in 2 .. 30 step 1)
+    {
+        println(iv)
+        val I_exp = iv / 2.0
+        val dynamic = HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6))
+
+        val config = Configuration()
+
+        val pathwayDynamic: Pathway<HHCellObject> = Pathway()
+        pathwayDynamic.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+        pathwayDynamic.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+        pathwayDynamic.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+        pathwayDynamic.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+        pathwayDynamic.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+
+
+        config.add(HHCellObject::class, arrayListOf(pathwayDynamic))
+
+        val s = Model(arrayListOf(dynamic), config)
+        s.simulate { it.currentTime() > 100000 }
 //
-//    val config = Configuration()
-//
-//    val pathwayHH: Pathway<HHCellObject> = Pathway()
-//    pathwayHH.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 10) { it.forCondition }
-//    pathwayHH.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 10) { it.forCondition }
-//    pathwayHH.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 10) { it.forCondition }
-//    pathwayHH.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 10) { it.forCondition }
-//    pathwayHH.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 10) { it.forCondition }
-//
-//    config.add(HHCellObject::class, arrayListOf(pathwayHH))
-//
-//    val s = Model(arrayListOf(hhCell), config)
-//    s.simulate { it.currentTime() > 100 }
+//        println((dynamic.signals as HHSignals))
+//        println((dynamic.history.size))
+
+        var fname = "i_${I_exp}.txt"
+
+        File(fname).bufferedWriter().use { out ->
+            out.write("I;V;N;M;H\n")
+            dynamic.history.forEach {
+                it as HHSignals
+                out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
+            }
+        }
+    }
 }
--- mads-experiments/src/main/kotlin/TestDynamicExperiment.kt
+++ mads-experiments/src/main/kotlin/TestDynamicExperiment.kt
@@ -1,84 +1,31 @@
 package domain
 
-import domain.mechanisms.*
+import domain.mechanisms.DynSignals
+import domain.mechanisms.simpleAddMechanism
+import domain.mechanisms.simpleDynamicMechanism
+import domain.mechanisms.simpleMechanism
 import domain.objects.DynamicObject
 import domain.objects.HHCellObject
-import domain.objects.HHSignals
 import org.jetbrains.research.mads.core.configuration.Configuration
 import org.jetbrains.research.mads.core.configuration.Pathway
 import org.jetbrains.research.mads.core.simulation.Model
-import java.io.File
 
 fun main() {
     createDynamicExperiment()
-    createDynamicExperimentMultipleI()
 }
 
-fun createDynamicExperiment()
-{
-    val I_exp = 8.0
-    val dynamic = HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6))
+fun createDynamicExperiment() {
+    val dynamic = DynamicObject(DynSignals())
 
     val config = Configuration()
 
-    val pathwayDynamic: Pathway<HHCellObject> = Pathway()
-    pathwayDynamic.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-    pathwayDynamic.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-    pathwayDynamic.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-    pathwayDynamic.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-    pathwayDynamic.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    val pathwayDynamic: Pathway<DynamicObject> = Pathway()
+    pathwayDynamic.add(DynamicObject::simpleDynamicMechanism, SimpleParameters(0.5), 10) { it.forCondition }
 
-    config.add(HHCellObject::class, arrayListOf(pathwayDynamic))
+    config.add(DynamicObject::class, arrayListOf(pathwayDynamic))
 
     val s = Model(arrayListOf(dynamic), config)
-    s.simulate { it.currentTime() > 100000 }
+    s.simulate { it.currentTime() > 100 }
 
-    var fname = "i_${I_exp}.txt"
-
-    File(fname).bufferedWriter().use { out ->
-        out.write("I;V;N;M;H\n")
-        dynamic.history.forEach {
-            it as HHSignals
-            out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
-        }
-    }
-}
-
-fun createDynamicExperimentMultipleI() {
-    for(iv in 2 .. 30 step 1)
-    {
-        println(iv)
-        val I_exp = iv / 2.0
-        val dynamic = HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6))
-
-        val config = Configuration()
-
-        val pathwayDynamic: Pathway<HHCellObject> = Pathway()
-        pathwayDynamic.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-        pathwayDynamic.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-        pathwayDynamic.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-        pathwayDynamic.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-        pathwayDynamic.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
-
-
-        config.add(HHCellObject::class, arrayListOf(pathwayDynamic))
-
-        val s = Model(arrayListOf(dynamic), config)
-        s.simulate { it.currentTime() > 100000 }
-//
-//        println((dynamic.signals as HHSignals))
-//        println((dynamic.history.size))
-
-        var fname = "i_${I_exp}.txt"
-
-        File(fname).bufferedWriter().use { out ->
-            out.write("I;V;N;M;H\n")
-            dynamic.history.forEach {
-                it as HHSignals
-                out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
-            }
-        }
-    }
-
-
+    println((dynamic.signals as DynSignals).x)
 }
