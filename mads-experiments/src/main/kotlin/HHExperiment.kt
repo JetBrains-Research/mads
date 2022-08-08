556a5cc0b218cc2355307a08ae2993a0790f8f7d
dvbozhko@gmail.com <dvbozhko@gmail.com>
2022-08-08 18:28:38 +0200

    [ADDED] 100 HH neurons simultaneously calculation experiment

--- mads-experiments/src/main/kotlin/HHExperiment.kt
+++ mads-experiments/src/main/kotlin/HHExperiment.kt
@@ -1,6 +1,7 @@
 package domain
 
 import domain.mechanisms.*
+import domain.objects.DynamicObject
 import domain.objects.HHCellObject
 import domain.objects.HHSignals
 import org.jetbrains.research.mads.core.configuration.Configuration
@@ -9,8 +10,9 @@ import org.jetbrains.research.mads.core.simulation.Model
 import java.io.File
 
 fun main() {
-    createHHCellsExperiment()
-    createDynamicExperimentMultipleI()
+//    createHHCellsExperiment()
+//    createDynamicExperimentMultipleI()
+    createHHHundredCellsExperiment()
 }
 
 fun createHHCellsExperiment()
@@ -73,6 +75,44 @@ fun createDynamicExperimentMultipleI() {
         File(fname).bufferedWriter().use { out ->
             out.write("I;V;N;M;H\n")
             dynamic.history.forEach {
+                it as HHSignals
+                out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
+            }
+        }
+    }
+}
+
+fun createHHHundredCellsExperiment()
+{
+    val I_exp = 8.0
+//    val dynamic = HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6))
+
+    val cells : ArrayList<HHCellObject> = arrayListOf()
+    for (i in 0..99) {
+        cells.add(HHCellObject(HHSignals(I = I_exp, V = -65.0, N = 0.32, M = 0.05, H= 0.6)))
+    }
+
+    val config = Configuration()
+
+    val pathwayDynamic: Pathway<HHCellObject> = Pathway()
+    pathwayDynamic.add(HHCellObject::IDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::VDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::NDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::MDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+    pathwayDynamic.add(HHCellObject::HDynamicMechanism, SimpleParameters(1.0), 2) { it.forCondition }
+
+    config.add(HHCellObject::class, arrayListOf(pathwayDynamic))
+
+    val s = Model(cells, config)
+    s.simulate { it.currentTime() > 100000 }
+
+    println("Already calculated")
+
+    for (i in 0..99) {
+        val fname = "${i}th_neuron.txt"
+        File(fname).bufferedWriter().use { out ->
+            out.write("I;V;N;M;H\n")
+            cells[i].history.forEach {
                 it as HHSignals
                 out.write("${it.I};${it.V}; ${it.N};${it.M};${it.H}\n")
             }
