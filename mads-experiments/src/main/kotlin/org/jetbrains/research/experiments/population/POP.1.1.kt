504b7329a1513e2d8a78dae19388eb34a91ede89
bbrh <875739+bbrh@users.noreply.github.com>
2023-03-24 15:07:40 +0100

    fix: randomizer

--- mads-experiments/src/main/kotlin/org/jetbrains/research/experiments/population/POP.1.1.kt
+++ mads-experiments/src/main/kotlin/org/jetbrains/research/experiments/population/POP.1.1.kt
@@ -28,7 +28,11 @@ enum class Connectedness {
 }
 
 fun main() {
+    pop_13_1()
+    pop_13_2()
     pop_13_3()
+    pop_13_4()
+    pop_13_5()
 //    val experimentName = "POP.13"
 //    val timePart = System.currentTimeMillis().toString()
 //    val noiseStds = arrayOf(2.0, 5.0, 7.0, 10.0)
@@ -78,7 +82,19 @@ fun pop_13_3(
     runSimulation(10.0,5.0,8,2,0.5,-1.0,Connectedness.CONSTANT, "$experimentName/$timePart")
 }
 
+fun pop_13_4(
+    experimentName:String="POP_13.4"
+) {
+    val timePart = System.currentTimeMillis().toString()
+    runSimulation(5.0,2.0,8,2,0.5,-1.0,Connectedness.RANDOM, "$experimentName/$timePart")
+}
 
+fun pop_13_5(
+    experimentName:String="POP_13.5"
+) {
+    val timePart = System.currentTimeMillis().toString()
+    runSimulation(5.0,2.0,80,20,0.5,-1.0,Connectedness.CONSTANT, "$experimentName/$timePart")
+}
 
 
 fun runSimulation(
@@ -101,12 +117,12 @@ fun runSimulation(
     )
     println("Experiment start time $startTime")
 
-    val dir = Path("log/${logPrefix}/izh/${noiseExc}-${noiseInh}/${nExc}-${nInh}/${synWeightExc}-${synWeightInh}/${connected}")
+    val dir = Path("log/${logPrefix}/izh/${noiseExc}-${noiseInh}/${nExc}-${nInh}"+
+            "/${synWeightExc}-${synWeightInh}/${connected}")
     val saver = FileSaver(dir)
     logSignals.forEach { saver.addSignalsNames(it) }
 
-    val rE = Random(randomSeed)
-    val rI = Random(randomSeed - 1)
+    val r = Random(randomSeed)
 
     val objects: ArrayList<ModelObject> = arrayListOf()
     val eNeurons: List<Neuron> = createPopulation(nExc, "excitatory") { ->
@@ -114,15 +130,15 @@ fun runSimulation(
             IzhConstants(
             a = 0.02,
             b = 0.2,
-            c = -65.0 + 15 * rE.nextDouble().pow(2),
-            d = 8.0 - 6.0 * rE.nextDouble().pow(2))
+            c = -65.0 + 15 * r.nextDouble().pow(2),
+            d = 8.0 - 6.0 * r.nextDouble().pow(2))
         )
     }
     val iNeurons: List<Neuron> = createPopulation(nInh, "inhibitory") { ->
         IzhNeuron(
             IzhConstants(
-            a = 0.02 + 0.08 * rI.nextDouble(),
-            b = 0.25 - 0.05 * rI.nextDouble(),
+            a = 0.02 + 0.08 * r.nextDouble(),
+            b = 0.25 - 0.05 * r.nextDouble(),
             c = -65.0,
             d = 2.0)
         )
@@ -130,20 +146,24 @@ fun runSimulation(
     objects.addAll(eNeurons)
     objects.addAll(iNeurons)
 
-    val eElectrodes = connectElectrodes(eNeurons) { seed: Long -> Electrode(Random(seed), CurrentSignals(I_e = 0.0), NoiseSignals(std = noiseExc)) }
-    val iElectrodes = connectElectrodes(iNeurons) { seed: Long -> Electrode(Random(seed), CurrentSignals(I_e = 0.0), NoiseSignals(std = noiseInh)) }
+    val eElectrodes = connectElectrodes(eNeurons)
+        { seed: Long -> Electrode(Random(seed), CurrentSignals(I_e = 0.0), NoiseSignals(std = noiseExc)) }
+    val iElectrodes = connectElectrodes(iNeurons)
+        { seed: Long -> Electrode(Random(seed), CurrentSignals(I_e = 0.0), NoiseSignals(std = noiseInh)) }
 
     objects.addAll(eElectrodes)
     objects.addAll(iElectrodes)
 
     if (connected != Connectedness.NONE) {
         val synapses: ArrayList<Synapse> = arrayListOf()
-        val randomExcComponent = if (connected==Connectedness.RANDOM) rE.nextDouble() else 1.0
-        val randomInhComponent = if (connected==Connectedness.RANDOM) rE.nextDouble() else 1.0
-        synapses.addAll(connectPopulations(eNeurons, eNeurons, weight = { synWeightExc * randomExcComponent }))
-        synapses.addAll(connectPopulations(eNeurons, iNeurons, weight = { synWeightExc * randomInhComponent }))
-        synapses.addAll(connectPopulations(iNeurons, eNeurons, weight = { synWeightInh * rI.nextDouble() }))
-        synapses.addAll(connectPopulations(iNeurons, iNeurons, weight = { synWeightInh * rI.nextDouble() }))
+        synapses.addAll(connectPopulations(eNeurons, eNeurons,
+            weight = { synWeightExc * if (connected==Connectedness.RANDOM) r.nextDouble() else 1.0 }))
+        synapses.addAll(connectPopulations(eNeurons, iNeurons,
+            weight = { synWeightExc * if (connected==Connectedness.RANDOM) r.nextDouble() else 1.0 }))
+        synapses.addAll(connectPopulations(iNeurons, eNeurons,
+            weight = { synWeightInh * if (connected==Connectedness.RANDOM) r.nextDouble() else 1.0 }))
+        synapses.addAll(connectPopulations(iNeurons, iNeurons,
+            weight = { synWeightInh * if (connected==Connectedness.RANDOM) r.nextDouble() else 1.0 }))
         objects.addAll(synapses)
     }
 
