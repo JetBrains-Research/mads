e3a9019ed7f071d239dc5ea2eb66361f165a986f
dvbozhko@gmail.com <dvbozhko@gmail.com>
2023-03-29 15:16:53 +0200

    [edited] build gradle for mads-core using serialization plugin

--- mads-core/build.gradle.kts
+++ mads-core/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm")
-    kotlin("plugin.serialization") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 description = "Core types for MADS"
