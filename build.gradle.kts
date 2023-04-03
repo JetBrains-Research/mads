ad60ca5fcbea040c73ee557399bb5573c73cd56d
dvbozhko@gmail.com <dvbozhko@gmail.com>
2023-04-03 14:35:06 +0200

    [edited] build script to create one jar which is possible to link using jitpack in github

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,66 @@
-group "org.jetbrains.research.mads"
-version "0.1.0"
+val tagName = System.getenv("GIT_TAG") ?: "unspecified"
+val javaVersion = JavaVersion.VERSION_17 // Adjust this to the desired Java version
+val javaLanguageVersion = JavaLanguageVersion.of(javaVersion.toString())
+val kotlinVersion = "1.8.0" // Adjust this to the desired Kotlin version
 
 plugins {
+    application
     java
-    idea
+    id("maven-publish")
     kotlin("jvm") version "1.8.0"
 }
 
+java {
+    toolchain {
+        languageVersion.set(javaLanguageVersion) // Set the desired JDK version for the toolchain
+    }
+}
+
+dependencies {
+    implementation(project("mads-core"))
+    implementation(project("mads-experiments"))
+    implementation(project("mads-ns"))
+
+}
+
+val fatJar = tasks.register<Jar>("fatJar") {
+    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+    archiveClassifier.set("all") // Naming the jar
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.compileClasspath.get()
+        .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+    from(contents)
+}
+
+tasks {
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("rootLibrary") {
+            artifact(fatJar) {
+                classifier = "all"
+            }
+
+            groupId = "org.jetbrains.research" // Replace with your group ID
+            artifactId = "mads" // Replace with your artifact ID
+            version = tagName // Replace with your version
+
+            from(components["java"])
+        }
+    }
+}
+
 allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+
     repositories {
         mavenCentral()
     }
@@ -15,12 +68,13 @@ allprojects {
     tasks {
         withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = javaVersion.toString()
             }
         }
+
+        withType<JavaCompile> {
+            sourceCompatibility = javaVersion.toString()
+            targetCompatibility = javaVersion.toString()
+        }
     }
-}
-
-dependencies {
-
 }
