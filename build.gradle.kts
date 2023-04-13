c0d524eff452c9f5559bc810eacab11fea5e93b4
Georgy Galumov <georgyg2010@gmail.com>
2023-04-13 20:46:36 +0700

    Gradle task to run experiment; fix of jar dependencies for JAR runs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,17 @@ val tagName = System.getenv("GIT_TAG") ?: "unspecified"
 val javaVersion = JavaVersion.VERSION_17 // Adjust this to the desired Java version
 val javaLanguageVersion = JavaLanguageVersion.of(javaVersion.toString())
 val kotlinVersion = "1.8.0" // Adjust this to the desired Kotlin version
+val kotlinCoroutineVersion = "1.6.2" // Adjust this to the desired Kotlin version
 
 plugins {
     java
     id("maven-publish")
     kotlin("jvm") version "1.8.0"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutineVersion")
 }
 
 java {
