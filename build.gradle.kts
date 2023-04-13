3db91302e8edf01168343f3614f34d39c3cdcb16
Georgy Galumov <georgyg2010@gmail.com>
2023-04-13 20:57:36 +0700

    turn back jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val kotlinVersion = "1.8.0" // Adjust this to the desired Kotlin version
 val kotlinCoroutineVersion = "1.6.2" // Adjust this to the desired Kotlin version
 
 plugins {
+    application
     java
     id("maven-publish")
     kotlin("jvm") version "1.8.0"
@@ -20,6 +21,32 @@ java {
         languageVersion.set(javaLanguageVersion) // Set the desired JDK version for the toolchain
     }
 }
+
+tasks {
+    jar {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+//        archiveClassifier.set("all") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.compileClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+}
+//
+//publishing {
+//    publications {
+//        create<MavenPublication>("rootLibrary") {
+//            groupId = "org.jetbrains.research" // Replace with your group ID
+//            artifactId = "mads" // Replace with your artifact ID
+//            version = tagName // Replace with your version
+//
+//            from(components["kotlin"])
+//        }
+//    }
+//}
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
