e6f695cb53fa4be0d0d67b7d1e9fbb285177cca4
dvbozhko@gmail.com <dvbozhko@gmail.com>
2023-05-31 18:32:28 +0200

    [edited] module renaming + excluding examples from build and publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 val tagName = System.getenv("GIT_TAG") ?: "unspecified"
+val groupName = "org.jetbrains.research.mads"
 val javaVersion = JavaVersion.VERSION_17 // Adjust this to the desired Java version
 val javaLanguageVersion = JavaLanguageVersion.of(javaVersion.toString())
 val kotlinVersion = "1.8.0" // Adjust this to the desired Kotlin version
@@ -18,6 +19,7 @@ java {
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     version = tagName
+    group = groupName
 
     repositories {
         mavenCentral()
@@ -45,5 +47,12 @@ subprojects {
             sourceCompatibility = javaVersion.toString()
             targetCompatibility = javaVersion.toString()
         }
+    }
+}
+
+// Exclude the example module from the library distribution
+afterEvaluate {
+    configurations.matching { it.name == "implementation" || it.name == "runtimeOnly" }.configureEach {
+        exclude(group = groupName, module = ":mads-examples")
     }
 }
